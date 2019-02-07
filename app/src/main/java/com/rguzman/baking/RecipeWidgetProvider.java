e96ab1afe292cdef96f760df1f55a02545fe6009
Raul Guzman <rguzmanc.161@gmail.com>
2019-02-07 14:03:55 -0500

    feat : update view for widget

--- app/src/main/java/com/rguzman/baking/RecipeWidgetProvider.java
+++ app/src/main/java/com/rguzman/baking/RecipeWidgetProvider.java
@@ -5,18 +5,18 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
 
+import com.rguzman.baking.domain.model.Recipe;
+
 /**
  * Implementation of App Widget functionality.
  */
 public class RecipeWidgetProvider extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    public static void updateAppWidget(Context context, Recipe recipe, AppWidgetManager appWidgetManager,
+                                       int[] appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget_provider);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.appwidget_text, recipe.getName());
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -24,10 +24,7 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
+
     }
 
     @Override
--- app/src/main/java/com/rguzman/baking/presentation/detail/RecipeDetailActivity.java
+++ app/src/main/java/com/rguzman/baking/presentation/detail/RecipeDetailActivity.java
@@ -1,11 +1,17 @@
 package com.rguzman.baking.presentation.detail;
 
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.rguzman.baking.R;
+import com.rguzman.baking.RecipeWidgetProvider;
 import com.rguzman.baking.domain.model.Recipe;
 import com.rguzman.baking.domain.model.Step;
 import com.rguzman.baking.presentation.detail.step.RecipeStepDetailFragment;
@@ -16,6 +22,8 @@ import dagger.android.support.DaggerAppCompatActivity;
 public class RecipeDetailActivity extends DaggerAppCompatActivity implements RecipeStepFragment.SelectStepListener {
 
     private static final String EXTRA_RECIPE = "com.rguzman.baking.extra.RECIPE";
+
+    private Recipe recipe;
 
     public static Intent getCallingIntent(Context context, Recipe recipe) {
         Intent intent = new Intent(context, RecipeDetailActivity.class);
@@ -29,7 +37,7 @@ public class RecipeDetailActivity extends DaggerAppCompatActivity implements Rec
         setContentView(R.layout.activity_recipe_detail);
 
         if (getIntent() != null && getIntent().hasExtra(EXTRA_RECIPE)) {
-            Recipe recipe = getIntent().getParcelableExtra(EXTRA_RECIPE);
+            recipe = getIntent().getParcelableExtra(EXTRA_RECIPE);
 
             setTitle(recipe.getName());
 
@@ -40,5 +48,27 @@ public class RecipeDetailActivity extends DaggerAppCompatActivity implements Rec
     @Override
     public void onSelectStep(Step step) {
         ActivityUtils.replaceFragment(this, R.id.recipe_step_detail_fragment, RecipeStepDetailFragment.newInstance(step));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.widget_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.widget:
+                if (item.isChecked()) {
+                    AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+                    int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, RecipeWidgetProvider.class));
+                    RecipeWidgetProvider.updateAppWidget(this, recipe, appWidgetManager, appWidgetIds);
+                }
+                item.setChecked(!item.isChecked());
+
+        }
+        return true;
     }
 }
