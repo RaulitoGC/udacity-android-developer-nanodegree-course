ffa079e5c6e6e6d9917673a027947939d1556b3f
Raul Guzman <rguzmanc.161@gmail.com>
2019-03-14 11:58:43 -0500

    feat: update gradle version and clean code in recipe list view

--- app/src/main/java/com/rguzman/baking/presentation/recipe/RecipeListActivity.java
+++ app/src/main/java/com/rguzman/baking/presentation/recipe/RecipeListActivity.java
@@ -1,13 +1,9 @@
 package com.rguzman.baking.presentation.recipe;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.VisibleForTesting;
-import android.support.test.espresso.IdlingResource;
 
 import com.rguzman.baking.R;
-import com.rguzman.baking.presentation.IdlingResource.SimpleIdlingResource;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
